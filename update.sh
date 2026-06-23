37550c21105392ede037bae22209e755e9722aa6
Liat Vax <liatvax@gmail.com>
2026-06-23 15:08:41 +0300

    Update portfolio 2026-06-23 15:08

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Run this from the Portfolio folder to push all changes to GitHub
 cd "$(dirname "$0")"
+# Keep index.html in sync with portfolio.html (GitHub Pages needs index.html)
+cp portfolio.html index.html
 git add -A
 git commit -m "Update portfolio $(date '+%Y-%m-%d %H:%M')"
 git push origin main
